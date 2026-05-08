a4085bed8df1e4c874ff07ebb5da2cf5f4b50ddf
Quocker20 <quocanh20705@gmail.com>
2026-05-09 05:28:24 +0700

    feat: Change from MySQL to PosgreSQL

--- src/main/resources/database/init.sql
+++ src/main/resources/database/init.sql
@@ -1,33 +1,30 @@
 -- hdiary_db.users definition
 
-CREATE TABLE `users`
+CREATE TABLE users
 (
-    `id`               bigint      NOT NULL AUTO_INCREMENT,
-    `username`         varchar(50) NOT NULL,
-    `display_name`     varchar(100) DEFAULT NULL,
-    `avatar_url`       varchar(255) DEFAULT NULL,
-    `avatar_public_id` varchar(100) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `idx_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+    id               BIGSERIAL PRIMARY KEY,
+    username         VARCHAR(50) NOT NULL UNIQUE,
+    display_name     VARCHAR(100) DEFAULT NULL,
+    avatar_url       VARCHAR(255) DEFAULT NULL,
+    avatar_public_id VARCHAR(100) DEFAULT NULL
+);
 
 -- hdiary_db.posts definition
 
-CREATE TABLE `posts`
+CREATE TABLE posts
 (
-    `id`              bigint      NOT NULL AUTO_INCREMENT,
-    `user_id`         bigint      NOT NULL,
-    `post_type`       enum('NORMAL','EVENT') DEFAULT 'NORMAL',
-    `event_category`  enum('SALARY','WEEKEND','REWARD','MOOD_UP','DATING') DEFAULT NULL,
-    `mood`            varchar(20) NOT NULL,
-    `content`         text,
-    `image_url`       varchar(255) DEFAULT NULL,
-    `image_public_id` varchar(100) DEFAULT NULL,
-    `created_at`      timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`),
-    KEY               `idx_user_created` (`user_id`,`created_at` DESC),
-    KEY               `idx_created_at` (`created_at` DESC),
-    KEY               `idx_mood` (`mood`),
-    CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    id              BIGSERIAL PRIMARY KEY,
+    user_id         BIGINT NOT NULL,
+    post_type       VARCHAR(20) DEFAULT 'NORMAL',
+    event_category  VARCHAR(20) DEFAULT NULL,
+    mood            VARCHAR(20) NOT NULL,
+    content         TEXT,
+    image_url       VARCHAR(255) DEFAULT NULL,
+    image_public_id VARCHAR(100) DEFAULT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_user_created ON posts (user_id, created_at DESC);
+CREATE INDEX idx_created_at ON posts (created_at DESC);
+CREATE INDEX idx_mood ON posts (mood);
